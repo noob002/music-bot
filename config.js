92b4b38c33ad74f0deccdbfbe0eba596cdc881e4
gkukl123 <92574906+noob002@users.noreply.github.com>
2024-02-25 20:02:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -26,9 +26,9 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "", //- The host name or IP of the lavalink server.
-			port: 80, // The port that lavalink is listening to. This must be a number!
-			password: "", //- The password of the lavalink server.
+			host: "lavalink.teramont.net", //- The host name or IP of the lavalink server.
+			port: 25565, // The port that lavalink is listening to. This must be a number!
+			password: "eHKuFcz67k4lBS64", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
@@ -40,8 +40,8 @@ module.exports = {
 		status: "online", //- You can have online, idle, dnd and invisible (Note: invisible makes people think the bot is offline)
 		activities: [
 			{
-				name: "Music", //- Status Text
-				type: "LISTENING", //- PLAYING, WATCHING, LISTENING, STREAMING
+				name: "🍽┆식당에서 알바", //- Status Text
+				type: "PLAYING", //- PLAYING, WATCHING, LISTENING, STREAMING
 			},
 		],
 	},
