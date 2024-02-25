e85ade640e18aa099200662ea52c585572a0f45a
gkukl123 <92574906+noob002@users.noreply.github.com>
2024-02-25 20:27:06 +0900

    Update config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ module.exports = {
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
 		},
 	],
-	embedColor: "#2f3136", //- Color of the embeds, hex supported
+	embedColor: "#D9D9D9", //- Color of the embeds, hex supported
 	presence: {
 		// PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
 		status: "online", //- You can have online, idle, dnd and invisible (Note: invisible makes people think the bot is offline)
